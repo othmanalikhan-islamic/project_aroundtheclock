5b1c39b3b36c14d7645af0c0e0e1714a83514aa7
OthmanEmpire <oz.alikhan@gmail.com>
2019-02-04 17:20:10 +0300

    Added the function T(a) with unit tests

--- aroundtheclock/prayertimes.py
+++ aroundtheclock/prayertimes.py
@@ -2,42 +2,10 @@
 Compute muslim prayer times based on astronomical formulae.
 """
 
-from datetime import datetime as dt
-from math import acos, asin, atan2, cos, radians, sin
+import datetime as dt
+from math import acos, asin, atan2, cos, degrees, radians, sin
 
 import julian
-
-
-def computeJulianDay(year, month, day):
-    """
-    Converts a Gregorian date to a Julian date.
-
-    :param year: Integer, the year.
-    :param month: Integer, the month.
-    :param day: Integer, the day.
-    :return: Float, the corresponding julian date.
-    """
-    return julian.to_jd(dt(year, month, day))
-
-
-# Julian Day (Virtual Reality and Augmented Reality: 15th EuroVR International Conference)
-def Julian(year, month, day):
-    d = (367 * year) - ((year + (int)((month + 9) / 12)) * 7 / 4) + (((int)(275 * month / 9)) + day - 730531.5);
-
-    g = radians(357.529 + 0.98560028* d);
-    q = radians(280.459 + 0.98564736* d);
-    L = radians(q + 1.915* sin(g) + 0.020* sin(2*g))
-
-    R = 1.00014 - 0.01671* cos(g) - 0.00014* cos(2*g)
-    e = radians(23.439 - 0.00000036* d)
-    RA = atan2(cos(e)* sin(L), cos(L))/ 15
-
-    D = asin(sin(e)* sin(L))
-    EqT = q/15 - RA
-    return D, EqT
-
-def T(angle, LAT, DEC):
-    return 1/15 * acos((-sin(angle) - sin(LAT)*sin(DEC)) / cos(LAT)*cos(DEC))
 
 
 def computeFajr(thuhr, angle, LAT, DEC, T):
@@ -47,5 +15,75 @@ def computeFajr(thuhr, angle, LAT, DEC, T):
     :return: datetime, the time for fajr.
     """
     print(T(angle, LAT, DEC))
-    fajr = thuhr - T(angle, LAT, DEC)
+    # return dt(2019, 1, 1)
+
+    t = T(angle, LAT, DEC)
+    hours, minutes = divmod(t*60, 60)
+    print(hours, minutes)
+    fajr = thuhr - dt(2019, 1, 1, int(hours), int(minutes))
     return fajr
+
+
+def computeJulianDay(year, month, day):
+    """
+    Converts a Gregorian date to a Julian date.
+
+    :param year: Integer, the year.
+    :param month: Integer, the month.
+    :param day: Integer, the day.
+    :return: Number, the corresponding julian date.
+    """
+    return julian.to_jd(dt.datetime(year, month, day))
+
+
+def computeSun(year, month, day):
+    """
+    Calculates the declination of the sun and the equation of time which are
+    needed in prayer calculations.
+
+    Equations:
+        Equation of Time = Apparent Solar Time - Mean Solar Time
+        Declination = The angle between the sun's rays and Earth's equator.
+
+    Abbreviations:
+        d - Number of days and fraction from 2000 January 1.5 (J2000.0)
+        g - Mean anomaly of the sun
+        q - Mean longitude of the sun
+        L - Geocentric apparent ecliptic longitude of the sun (adjusted for aberration)
+        e - Mean obliquity of the ecliptic
+        RA - Right ascension
+
+    :param year: Integer, the year.
+    :param month: Integer, the month.
+    :param day: Integer, the day.
+    :return: 2-Tuple, (declination, equation of time)
+    """
+    d = computeJulianDay(year, month, day) - 2451545.0
+
+    g = radians((357.529 + 0.98560028 * d) % 360)
+    q = radians((280.459 + 0.98564736 * d) % 360)
+    L = radians((degrees(q) + 1.915 * sin(g) + 0.020 * sin(2*g)))
+
+    e = radians((23.439 - 0.00000036 * d) % 360)
+    RA = degrees(atan2(cos(e) * sin(L), cos(L))) / 15
+
+    declination = degrees(asin(sin(e) * sin(L)))
+    equationOfTime = degrees(q)/15 - (RA % 24)
+    return declination, equationOfTime
+
+
+def T(angle, latitude, declination):
+    """
+    The equation that computes the time taken for the sun to reach a given
+    angle below the horizon.
+
+    :param angle: Number, the angle in degrees.
+    :param latitude: Number, the latitude in degrees.
+    :param declination: Number, the latitude in degrees.
+    :return: datetime.timedelta, the time taken for the sun to reach the angle.
+    """
+    a = radians(angle)
+    LAT = radians(latitude)
+    DEC = radians(declination)
+    t = 1/15 * degrees(acos((-sin(a) - sin(LAT)*sin(DEC)) / (cos(LAT)*cos(DEC))))
+    return dt.timedelta(hours=t)
--- aroundtheclock/test_prayer.py
+++ aroundtheclock/test_prayer.py
@@ -1,5 +1,6 @@
 import unittest
-from datetime import datetime as dt
+import datetime as dt
+from pprint import pprint
 
 import prayertimes
 from math import radians
@@ -12,22 +13,39 @@ class TestPrayerModule(unittest.TestCase):
         self.LAT = radians(26.2172)
         self.LON = radians(50.1971)
 
-    def testComputeJulianDay(self):
+    def testComputeJulianDay_standardDate_convertedDateToFloat(self):
         jd = 2458517.5
         self.assertEqual(jd, prayertimes.computeJulianDay(2019, 2, 3))
 
+    def testComputeSun_standardValue_returnCalculatedValue(self):
+        # declination = -16.668351110659202
+        # equationOfTime = -0.22864750125837574
+        declination = -23.03993184124207
+        equationOfTime = -0.053431595269049836
+        D, EoT = prayertimes.computeSun(2019, 1, 1)
+        self.assertEqual(D, declination, msg="Declination calculations failed!")
+        self.assertEqual(EoT, equationOfTime, msg="EoT calculations failed!")
+
+    def testT_standardValue_returnCalculatedValue(self):
+        calculated = dt.timedelta(hours=6.146957845647092)
+        angle = -18.5
+        LAT = 26.2172
+        DEC = 50.1971
+        self.assertEqual(calculated, prayertimes.T(angle, LAT, DEC))
+
     @unittest.skip
     def testComputeFajr(self):
-        angle = radians(18.5)
-        thuhr = dt.strptime("2019-01-01 11:53", self.FORMAT)
+        angle = radians(-18.5)
+        thuhr = dt.datetime.strptime("2019-01-01 11:53", self.FORMAT)
         fajr = dt.strptime("2019-01-01 05:01", self.FORMAT)
 
-        D, _ = prayertimes.Julian(2019, 1, 1)
-
+        D, _ = prayertimes.computeSun(2019, 1, 1)
         prayer = prayertimes.computeFajr(thuhr, angle, self.LAT, D, prayertimes.T)
-        self.assertAlmostEqualPrayer(prayer, fajr, 5)
+        pprint(prayer)
 
-    def assertAlmostEqualPrayer(self, p1, p2, error):
+        self.assertAlmostEqualPrayer(fajr, prayer, 5)
+
+    def assertAlmostEqualPrayer(self, p1, p2, err):
         """
         Tests whether the two prayer times are almost equal by:
             Check
@@ -40,5 +58,5 @@ class TestPrayerModule(unittest.TestCase):
         diff = p2 - p1
         hours, _ = divmod(diff.total_seconds(), 3600)
         minutes, _ = divmod(diff.total_seconds(), 60)
-        self.assertEqual(hours, 0)
-        self.assertTrue(abs(minutes) <= error)
+        self.assertEqual(hours, 0, msg="Prayer time differs in hours!")
+        self.assertTrue(abs(minutes) <= err, msg="Prayer time differs by more than {} min!".format(err))

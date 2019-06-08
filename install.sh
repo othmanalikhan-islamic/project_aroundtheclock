f86e2d8b857cfcc2d3dc248783e534e4df1f8679
Othman Alikhan <oz.alikhan@gmail.com>
2019-06-08 14:34:23 +0300

    Updated install.sh to no longer require user YES prompting

--- install.sh
+++ install.sh
@@ -1,14 +1,15 @@
+#!/usr/bin/env bash
 echo -e "Installing 'Python' dependencies!\n"
-pip3 install -r requirements.txt
+yes | pip3 install -r requirements.txt
 
 echo -e "Installing 'dnsiff' dependency!\n"
-sudo apt-get install dsniff             # To block internet via 'arpspoof' module
+yes | sudo apt-get install dsniff             # To block internet via 'arpspoof' module
 
 echo -e "Installing 'supervisor' dependency!\n"
 PATH_SRC=./supervisord/aroundtheclock.conf
 PATH_DST=/etc/supervisor/conf.d/aroundtheclock.conf
-sudo apt-get install supervisor         # To ensure reliability of running script
-cp $PATH_SRC $PATH_DST
+yes | sudo apt-get install supervisor         # To ensure reliability of running script
+sudo cp $PATH_SRC $PATH_DST
 read -p "Please hit ENTER to modify the paths for the 'supervisorctl' config file!"
 vi $PATH_DST
 echo -e "The config file located at $PATH_DST has been modified!\n"

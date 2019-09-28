a8aab9054c26d1b4770adddae814ad49b1ccc4e5
OthmanEmpire <oz.alikhan@gmail.com>
2019-09-28 16:31:40 +0300

    Ignore this commit message (travis debugging purposes)

--- test/requirements_test_linux.sh
+++ test/requirements_test_linux.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+echo -e "\n>>> CREATING BATS DIRECTORY IN HOME DIRECTORY..."
 cd ~ || exit
+mkdir bats
+cd bats || exit
+
+echo -e "\n>>> CLONING GITHUB BATS REPOSITORY..."
 git clone https://github.com/sstephenson/bats.git
 cd bats || exit
-./install.sh /usr/local
+
+echo -e "\n>>> INSTALLING BATS..."
+sudo ./install.sh /usr/local
